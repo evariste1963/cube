07b3cb75f188edc530aaa78d5a5b5603086ddbcf
evariste1963 <evariste@hotmail.co.uk>
2022-02-06 15:49:14 +0000

    rotation concept sorted -- rotate cube onn Y axis

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const cube = document.querySelector(".cube");
+
+const contactBtn = document.querySelector(".contact-btn");
+const signupBtn1 = document.querySelector(".signup-btn1");
+const contactBtn1 = document.querySelector(".contact-btn1");
+
+contactBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  cube.style.transform = "rotateY(90deg)";
+});
+
+contactBtn1.addEventListener("click", (e) => {
+  e.preventDefault();
+  cube.style.transform = "rotateY(0deg)";
+});
+
+signupBtn1.addEventListener("click", (e) => {
+  e.preventDefault();
+  cube.style.transform = "rotateY(-90deg)";
+});
