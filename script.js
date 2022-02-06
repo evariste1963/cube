813f5d336355fded49b56faea514c863bb83b6dc
evariste1963 <evariste@hotmail.co.uk>
2022-02-06 21:57:40 +0000

    can't quite get the rotation right

--- script.js
+++ script.js
@@ -1,23 +1,80 @@
 "use strict";
 
-const cube = document.querySelector(".cube");
+const cube = document.querySelector("#cube");
 
-const contactBtn = document.querySelector(".contact-btn");
-const signupBtn1 = document.querySelector(".signup-btn1");
-const contactBtn1 = document.querySelector(".contact-btn1");
+const menuBtnR = document.querySelector(".menu-btnR");
+const menuBtnL = document.querySelector(".menu-btnL");
+const contactBtnR = document.querySelector(".contact-btnR");
+const contactBtnL = document.querySelector(".contact-btnL");
+const signinBtnR = document.querySelector(".signin-btnR");
+const signinBtnL = document.querySelector(".signin-btnL");
+const signupBtnR = document.querySelector(".signup-btnR");
+const signupBtnL = document.querySelector(".signup-btnL");
 
-contactBtn.addEventListener("click", (e) => {
+contactBtnL.addEventListener("click", (e) => {
   e.preventDefault();
+
   cube.style.transform = "rotateY(90deg)";
+});
+contactBtnR.addEventListener("click", (e) => {
+  e.preventDefault();
+  cube.style.transform = "rotateY(-270deg)";
+});
+
+signinBtnL.addEventListener("click", (e) => {
+  e.preventDefault();
+  cube.style.transform = "rotateY(180deg)";
+  console.log("clicked");
+});
+signinBtnR.addEventListener("click", (e) => {
+  e.preventDefault();
+  cube.style.transform = "rotateY(-180deg)";
+  
   console.log("clicked");
 });
 
-// contactBtn1.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   cube.style.transform = "rotateY(0deg)";
-// });
-
-signupBtn1.addEventListener("click", (e) => {
+signupBtnL.addEventListener("click", (e) => {
+  e.preventDefault();
+  cube.style.transform = "rotateY(270deg)";
+  console.log("clicked");
+});
+signupBtnR.addEventListener("click", (e) => {
   e.preventDefault();
   cube.style.transform = "rotateY(-90deg)";
+  console.log("clicked");
 });
+
+menuBtnL.addEventListener("click", (e) => {
+  e.preventDefault();
+  cube.style.transform = "rotateY(0deg)";
+  console.log("clicked");
+});
+menuBtnR.addEventListener("click", (e) => {
+  e.preventDefault();
+  cube.style.transform = "rotateY(0deg)";
+  console.log("clicked");
+});
+
+var el = document.getElementById("cube");
+
+var st = window.getComputedStyle(el, null);
+
+var tr =
+  st.getPropertyValue("-webkit-transform") ||
+  st.getPropertyValue("-moz-transform") ||
+  st.getPropertyValue("-ms-transform") ||
+  st.getPropertyValue("-o-transform") ||
+  st.getPropertyValue("transform") ||
+  "Either no transform set, or browser doesn't do getComputedStyle";
+  console.log(tr);
+var values = tr.split("(")[1],
+  values = values.split(")")[0],
+  values = values.split(",");
+
+var a = values[0]; // 0.866025
+var b = values[1]; // 0.5
+var c = values[2]; // -0.5
+var d = values[3]; // 0.866025
+
+var angle = Math.round(Math.asin(b) * (180 / Math.PI));
+console.log(angle);
