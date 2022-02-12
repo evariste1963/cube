2372fdee1a28c98d471f3b46504fecf1f6c77c2a
evariste1963 <evariste@hotmail.co.uk>
2022-02-12 15:10:44 +0000

    onload instead of IFFE

--- script.js
+++ script.js
@@ -4,21 +4,18 @@ const outerCube = document.querySelector("#outer-cube");
 const innerCube = document.querySelector("#inner-cube");
 const centerCube = document.querySelector("#center-cube");
 
+window.onload = function () {
+  outerCube.animate(
+    [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
+    { duration: 10000, iterations: Infinity }
+  );
+  innerCube.animate(
+    [{ transform: "rotateY(0)" }, { transform: "rotateY(360deg)" }],
+    { duration: 5000, iterations: Infinity }
+  );
 
-  //IIFE -- replaces keyframes in CSS
-  (function () {
-    outerCube.animate(
-      [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
-      { duration: 10000, iterations: Infinity }
-    );
-    innerCube.animate(
-      [{ transform: "rotateY(0)" }, { transform: "rotateY(360deg)" }],
-      { duration: 5000, iterations: Infinity }
-    );
-
-    centerCube.animate(
-      [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
-      { duration: 3000, iterations: Infinity }
-    );
-  }
-)();
+  centerCube.animate(
+    [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
+    { duration: 3000, iterations: Infinity }
+  );
+};
