70323eca55b8d9a934e2ba508807488f5d722941
evariste1963 <evariste@hotmail.co.uk>
2022-02-12 20:35:13 +0000

    using setInterval instead of animate -- could be used on something in the middle

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const outerCube = document.querySelector("#outer-cube");
-const innerCube = document.querySelector("#inner-cube");
+const innerCube = document.getElementById("inner-cube");
 const centerCube = document.querySelector("#center-cube");
 
 window.onload = function () {
@@ -9,13 +9,24 @@ window.onload = function () {
     [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
     { duration: 10000, iterations: Infinity }
   );
-  innerCube.animate(
-    [{ transform: "rotateY(0)" }, { transform: "rotateY(360deg)" }],
-    { duration: 5000, iterations: Infinity }
-  );
+  // innerCube.animate(
+  //   [{ transform: "rotateY(0)" }, { transform: "rotateY(360deg)" }],
+  //   { duration: 5000, iterations: Infinity }
+  // );
 
   centerCube.animate(
     [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
     { duration: 3000, iterations: Infinity }
   );
+};
+
+//using setInterval instead of above
+let angle = 0;
+setInterval(function () {
+  angle >= 360 ? (angle = 0) : innerSpin(angle);
+  angle++;
+}, 14);
+
+const innerSpin = function (angle) {
+  innerCube.style.transform = `rotateY(${angle}deg)`;
 };
