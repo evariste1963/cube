6ffd730e85b3736f674ebd4e8baf651c4419820b
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-02-14 13:31:24 +0000

    setInterval 2

--- script.js
+++ script.js
@@ -4,26 +4,30 @@ const outerCube = document.querySelector("#outer-cube");
 const innerCube = document.getElementById("inner-cube");
 const centerCube = document.querySelector("#center-cube");
 
+const slow = 10000;
+const medium = slow * 0.6;
+const fast = medium * 0.6;
+
 //3 ways to crack the same egg
 //JS version of keyframes with added onload function call
 window.onload = function () {
   outerCube.animate(
     [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
-    { duration: 10000, iterations: Infinity }
+    { duration: slow, iterations: Infinity }
   );
 };
 
-//same as above but using requestAnimationFrame as a 1 shot infonite call, which is a bit meaningless apparantly
+//same as above but using requestAnimationFrame as a 1 shot infinite call, which is a bit meaningless apparantly
 const center = function () {
   centerCube.animate(
     [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
-    { duration: 3000, iterations: Infinity }
+    { duration: fast, iterations: Infinity }
   );
 };
 window.requestAnimationFrame(center);
 
 //using setInterval -- doesn't require calling
-let angle = 0;
+/*let angle = 0;
 setInterval(function () {
   angle >= 360 ? (angle = 0) : innerSpin(angle);
   angle++;
@@ -31,4 +35,14 @@ setInterval(function () {
 
 const innerSpin = function (angle) {
   innerCube.style.transform = `rotateY(${angle}deg)`;
-};
+};*/
+
+//same as above but different
+let angle = 0;
+function innerSpin() {
+  angle >= 360
+    ? (angle = 0)
+    : (innerCube.style.transform = `rotateY(${angle}deg)`);
+  angle++;
+}
+setInterval(innerSpin, 14);
