83b33551b9ddd70770ff1930a913942c5c2b3057
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-02-14 12:58:41 +0000

    js comments again

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const outerCube = document.querySelector("#outer-cube");
 const innerCube = document.getElementById("inner-cube");
 const centerCube = document.querySelector("#center-cube");
 
+//3 ways to crack the same egg
+//JS version of keyframes with added onload function call
 window.onload = function () {
   outerCube.animate(
     [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
     { duration: 10000, iterations: Infinity }
   );
-  // innerCube.animate(
-  //   [{ transform: "rotateY(0)" }, { transform: "rotateY(360deg)" }],
-  //   { duration: 5000, iterations: Infinity }
-  // );
 };
+
+//same as above but using requestAnimationFrame as a 1 shot infonite call, which is a bit meaningless apparantly
 const center = function () {
   centerCube.animate(
     [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
@@ -22,7 +22,7 @@ const center = function () {
 };
 window.requestAnimationFrame(center);
 
-//using setInterval instead of above
+//using setInterval -- doesn't require calling
 let angle = 0;
 setInterval(function () {
   angle >= 360 ? (angle = 0) : innerSpin(angle);
