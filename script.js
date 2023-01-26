7c0fd0a9f8be2c0cf2d67d24c9ea9b3a1a01371c
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2023-01-26 08:36:20 +0000

    center ball fiddling

--- script.js
+++ script.js
@@ -15,8 +15,13 @@ window.onload = function () {
     [{ transform: "rotateY(0)" }, { transform: "rotateY(-360deg)" }],
     { duration: slow, iterations: Infinity }
   );
-  ball.animate({transform: "scale(40, 40)"},
-  { duration: 500, iterations: Infinity })
+  ball.animate(
+    { transform: "scale(70, 70)" },
+    {
+      duration: 1300,
+      iterations: Infinity,
+    }
+  );
 };
 
 //same as above but using requestAnimationFrame as a 1 shot infinite call, which is a bit meaningless apparantly
